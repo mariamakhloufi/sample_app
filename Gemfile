b4f9f9864187764d58fa907d962ec83dbffee556
Maria <maria.makhloufi@gmail.com>
2013-09-05 13:44:38 -0700

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 gem 'rails', '3.2.13'
 
+gem 'rubyzip',  "~> 0.9.9"
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/help"
 
+  get "static_pages/about"
+
   match '/home',    :to => 'static_pages#home'
 
   match '/help',    :to => 'static_pages#help'
+
+  match '/about',    :to => 'static_pages#about'
  
 
   # The priority is based upon order of creation:
